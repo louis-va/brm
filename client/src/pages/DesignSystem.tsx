15de91828dd31ff88aee5ca3e2ff52f10ecc8642
bSchutters <bryan021@hotmail.be>
2023-11-29 22:08:24 +0100

    add button component

--- client/src/pages/DesignSystem.tsx
+++ client/src/pages/DesignSystem.tsx
@@ -1,8 +1,9 @@
+import { Button } from "../ui/design-system/Button";
 import { Typography } from "../ui/design-system/Typography";
 
 export default function DesignSystem() {
   return (
-    <div className="bg-blackPrimary w-screen h-screen p-30">
+    <div className="bg-blackPrimary w-screen h-screen p-30 flex flex-col gap-50">
       <div className="p-30 bg-orangePrimary rounded-25">
         <Typography
           fontSize="96"
@@ -35,6 +36,28 @@ export default function DesignSystem() {
         <Typography fontSize="20" textColor="black" fontFamily="Franklin">
           Texte 2
         </Typography>
+      </div>
+      <div className="p-30 bg-orangePrimary rounded-25 flex gap-10">
+        <Button variant="orange">
+          <Typography fontSize="20" textColor="black" fontFamily="Franklin">
+            Bouton 1
+          </Typography>
+        </Button>
+        <Button variant="black">
+          <Typography fontSize="20" textColor="orange" fontFamily="Franklin">
+            Bouton 2
+          </Typography>
+        </Button>
+        <Button variant="orange" icon>
+          <Typography fontSize="20" textColor="black" fontFamily="Franklin">
+            Bouton 3
+          </Typography>
+        </Button>
+        <Button variant="black" icon>
+          <Typography fontSize="20" textColor="orange" fontFamily="Franklin">
+            Test
+          </Typography>
+        </Button>
       </div>
     </div>
   );
