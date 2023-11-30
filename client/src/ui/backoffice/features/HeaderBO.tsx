ac0963bd72af1c23ba00c11941fbf6464296026c
bSchutters <bryan021@hotmail.be>
2023-11-30 15:22:31 +0100

    fix de l'importation du logo

--- client/src/ui/backoffice/features/HeaderBO.tsx
+++ client/src/ui/backoffice/features/HeaderBO.tsx
@@ -1,27 +1,37 @@
-import {Logo} from "../../design-system/backoffice/Logo";
-import {Button} from "../../design-system/Button.tsx";
-import {Typography} from "../../design-system/Typography.tsx";
+import { Logo } from "../../design-system/Logo.tsx";
+import { Button } from "../../design-system/Button.tsx";
+import { Typography } from "../../design-system/Typography.tsx";
 
 interface HeaderBOProps {
-    className?: string;
+  className?: string;
 }
 
-export const HeaderBo = ({className}: HeaderBOProps) => {
-    return (
-        <header className={`${className} w-full h-[105px] bg-orangePrimary flex flex-row flex-nowrap justify-between items-center`}>
-            <Logo className={'ml-10'} size={"small"}/>
-            <div className={'mr-95'}>
-                <Button icon={false} variant={'black'} type={'button'}>
-                    <Typography fontSize={'20'} textColor={'white'} fontFamily={'Franklin'}>
-                        Vers le site
-                    </Typography>
-                </Button>
-                <Button className={'ml-4'} icon={true} variant='orange' type='button'>
-                    <Typography fontSize={'20'} textColor={'black'} fontFamily={'Franklin'}>
-                        Admin
-                    </Typography>
-                </Button>
-            </div>
-        </header>
-    )
-}
+export const HeaderBo = ({ className }: HeaderBOProps) => {
+  return (
+    <header
+      className={`${className} w-full h-[105px] bg-orangePrimary flex flex-row flex-nowrap justify-between items-center`}
+    >
+      <Logo className={"ml-10"} size={"small"} />
+      <div className={"mr-95"}>
+        <Button icon={false} variant={"black"} type={"button"}>
+          <Typography
+            fontSize={"20"}
+            textColor={"white"}
+            fontFamily={"Franklin"}
+          >
+            Vers le site
+          </Typography>
+        </Button>
+        <Button className={"ml-4"} icon={true} variant="orange" type="button">
+          <Typography
+            fontSize={"20"}
+            textColor={"black"}
+            fontFamily={"Franklin"}
+          >
+            Admin
+          </Typography>
+        </Button>
+      </div>
+    </header>
+  );
+};
