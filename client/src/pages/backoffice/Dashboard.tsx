739748650d2fe2a16217398a4889aac86a2988ee
alexander-fejari <alexander.fejari@gmail.com>
2023-11-30 14:48:20 +0100

    backoffice/feature/header

--- client/src/pages/backoffice/Dashboard.tsx
+++ client/src/pages/backoffice/Dashboard.tsx
@@ -3,9 +3,7 @@ import {HeaderBo} from "../../ui/backoffice/features/HeaderBO.tsx";
 export default function Dashboard() {
     return (
         <>
-            <section>
-                <HeaderBo></HeaderBo>
-            </section>
+            <HeaderBo></HeaderBo>
         </>
     )
 }
