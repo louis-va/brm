d1f1fbc5b7eea3ecbb25082df5a46042891f28b9
bSchutters <bryan021@hotmail.be>
2023-11-30 11:06:27 +0100

    update frontend btnPlusMinus

--- client/src/ui/design-system/BtnPlusMinus.tsx
+++ client/src/ui/design-system/BtnPlusMinus.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "./Typography";
+import { FaPlus, FaMinus } from "react-icons/fa";
 
 interface BtnPlusMinusProps {
   plus?: boolean;
@@ -10,15 +10,7 @@ export const BtnPlusMinus = ({ plus, className }: BtnPlusMinusProps) => {
     <button
       className={`bg-blackPrimary text-whitePrimary flex items-center justify-center w-[20px] h-[20px] rounded-full font-Franklin ${className}`}
     >
-      {plus ? (
-        <Typography fontFamily="Franklin" fontSize="20" textColor="white">
-          +
-        </Typography>
-      ) : (
-        <Typography fontFamily="Franklin" fontSize="20" textColor="white">
-          -
-        </Typography>
-      )}
+      {plus ? <FaPlus size="6px" /> : <FaMinus size="6px" />}
     </button>
   );
 };
