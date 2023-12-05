e5073254ec06451f29f973319f26d5495ccf56c0
Alexander-fejari <alexander.fejari@gmail.com>
2023-12-05 19:47:11 +0100

    review header Backoffice

--- client/src/ui/backoffice/features/HeaderBO.tsx
+++ client/src/ui/backoffice/features/HeaderBO.tsx
@@ -1,21 +1,25 @@
 import {Logo} from '../../design-system/Logo.tsx'
 import {Button} from "../../design-system/Button.tsx";
 import {Typography} from "../../design-system/Typography.tsx";
+import CustomLink from "../design-system/CustomLink.tsx";
 
 
 interface HeaderBOProps {
-  className?: string;
+    className?: string;
 }
 
 export const HeaderBo = ({className}: HeaderBOProps) => {
     return (
-        <header className={`${className} w-full h-[100px] bg-orangePrimary flex flex-row flex-nowrap justify-between items-center`}>
+        <header
+            className={`${className} w-full h-[100px] bg-orangePrimary flex flex-row flex-nowrap justify-between items-center`}>
             <Logo className={'ml-10'} size={"small"}/>
             <div className={'mr-95'}>
                 <Button icon={false} variant={'black'} type={'button'}>
-                    <Typography fontSize={'20'} textColor={'white'} fontFamily={'Franklin'}>
-                        Vers le site
-                    </Typography>
+                    <CustomLink to={'/'}>
+                        <Typography fontSize={'20'} textColor={'white'} fontFamily={'Franklin'}>
+                            Vers le site
+                        </Typography>
+                    </CustomLink>
                 </Button>
                 <Button className={'ml-4'} icon={true} variant='orange' type='button'>
                     <Typography fontSize={'20'} textColor={'black'} fontFamily={'Franklin'}>
