96dbef4cf24027169bae8b98404d755c02b911ae
Alexander-fejari <alexander.fejari@gmail.com>
2023-12-06 13:20:11 +0100

    modif layout dashboard + creation files card

--- client/src/pages/backoffice/Dashboard.tsx
+++ client/src/pages/backoffice/Dashboard.tsx
@@ -3,9 +3,15 @@ import {HeaderVertical} from "../../ui/backoffice/features/HeaderVertical.tsx";
 
 export default function Dashboard() {
     return (
-        <div className="w-full h-screen flex flex-col">
-            <HeaderBo></HeaderBo>
-            <HeaderVertical></HeaderVertical>
+        <div className="grid grid-cols-5 grid-rows-[100px,1fr]  h-screen">
+            <div className="col-span-5">
+                <HeaderBo></HeaderBo>
+            </div>
+            <div className="row-span-4 row-start-2">
+                <HeaderVertical></HeaderVertical>
+            </div>
+            <div className="col-span-4 row-span-4 row-start-2">3</div>
         </div>
+    
     )
 }
