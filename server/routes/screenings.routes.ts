19f9499ac4311867599bd1b0cb657e74e17c3044
Louis Van Aken <vanakenlouis@gmail.com>
2023-12-07 00:46:09 +0100

    Add get all screenings feature

--- server/routes/screenings.routes.ts
+++ server/routes/screenings.routes.ts
@@ -2,12 +2,21 @@ import express from 'express';
 
 import controller from '../controllers/screenings.controller'
 import verifyScreening from '../middlewares/verifyScreening'
+import auth from '../middlewares/verifyAuth'
 
 const router = express.Router();
 
 router.post("/add",
-  [verifyScreening.checkDate],
+  [
+    auth.verifyToken, 
+    auth.isAdmin, 
+    verifyScreening.checkDate 
+  ],
   controller.addScreening
+);
+
+router.get("/",
+  controller.getAllScreenings
 );
 
 export default router;
