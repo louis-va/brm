12269d6de47886033d8882279221bdde2130b7ad
Louis Van Aken <vanakenlouis@gmail.com>
2023-12-08 17:43:23 +0100

    Edit file name

--- server/index.ts
+++ server/index.ts
@@ -8,8 +8,8 @@ import cookieSession from 'cookie-session';
 // Import routes
 import database from './models';
 import authRoutes from './routes/auth.routes'
-import screeningRoutes from './routes/screenings.routes'
-import bookingRoutes from './routes/bookings.routes'
+import screeningRoutes from './routes/screening.routes'
+import bookingRoutes from './routes/booking.routes'
 
 // ENV variables
 dotenv.config();
--- server/routes/booking.routes.ts
+++ server/routes/booking.routes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import controller from '../controllers/bookings.controller'
+import controller from '../controllers/booking.controller'
 import auth from '../middlewares/verifyAuth'
 
 const router = express.Router();
