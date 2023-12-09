a534d306498d4e4b5263f834e4d478631fa3757c
Louis Van Aken <vanakenlouis@gmail.com>
2023-12-09 13:44:52 +0100

    Add booking validation

--- server/routes/booking.routes.ts
+++ server/routes/booking.routes.ts
@@ -3,13 +3,16 @@ import express from 'express';
 import controller from '../controllers/booking.controller'
 import auth from '../middlewares/verifyAuth'
 import verifyScreening from '../middlewares/verifyScreening';
+import verifyBooking from '../middlewares/verifyBooking';
 
 const router = express.Router();
 
 router.post("/add",
   [
     auth.verifyToken,
-    verifyScreening.checkScreeningId
+    verifyScreening.checkScreeningId,
+    verifyBooking.validateSeats,
+    verifyBooking.checkSeatsDisponibility
   ],
   controller.addBooking
 );
