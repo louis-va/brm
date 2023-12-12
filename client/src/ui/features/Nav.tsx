bd70f18da70d9b510f95a0aa636f20d924880bd7
bSchutters <bryan021@hotmail.be>
2023-12-12 16:55:24 +0100

    add link to logo to get back home

--- client/src/ui/features/Nav.tsx
+++ client/src/ui/features/Nav.tsx
@@ -2,6 +2,7 @@ import { Logo } from "../design-system/Logo.tsx";
 import { Button } from "../design-system/Button.tsx";
 import { Typography } from "../design-system/Typography.tsx";
 import { FaAngleDown } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 interface NavProps {
   className?: string;
@@ -12,14 +13,15 @@ export const Nav = ({ className }: NavProps) => {
     <nav
       className={`${className} sticky top-0 w-full h-fit p-30 bg-orangePrimary flex flex-row justify-between items-center z-50`}
     >
-      <Logo size={"small"} />
+      <Link to={"/"}>
+        <Logo size={"small"} />
+      </Link>
       <div className="flex gap-5">
         <Button variant="black">
           <Typography fontSize="20" textColor="orange" fontFamily="Franklin">
             Date
           </Typography>
           <FaAngleDown size="15px" />
-
         </Button>
         <Button variant="black">
           <Typography fontSize="20" textColor="orange" fontFamily="Franklin">
