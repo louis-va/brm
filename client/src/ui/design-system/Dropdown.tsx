f765832a1fc9d2f4efcf3a0cce14d93dd2012806
bSchutters <bryan021@hotmail.be>
2023-12-15 15:02:53 +0100

    update design dropdown

--- client/src/ui/design-system/Dropdown.tsx
+++ client/src/ui/design-system/Dropdown.tsx
@@ -1,109 +1,147 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from "react";
 import { Typography } from "./Typography.tsx";
-import { RxCaretDown } from "react-icons/rx";
+import { RxCaretDown, RxCaretUp } from "react-icons/rx";
 
 interface DropdownProps {
-    label: string;
-    variant: 'orange' | 'black';
-    onSelect: (selectedOption: string) => void;
-    className?: string;
-    formatDates?: boolean
+  label: string;
+  variant: "orange" | "black";
+  onSelect: (selectedOption: string) => void;
+  className?: string;
+  formatDates?: boolean;
 }
 
-const Dropdown = ({ label, variant, onSelect, formatDates,className }: DropdownProps) => {
-    const [options, setOptions] = useState<string[]>([]);
-    const [isOpen, setIsOpen] = useState(false);
-    const [selectedOption, setSelectedOption] = useState('');
-    const dropdownRef = useRef(null);
-    
-    useEffect(() => {
-        console.log('Dropdown component mounted');
-        // Fetch options based on the label
-        fetch(`https://api.brm.lou-va.com/screenings/${label.toLowerCase()}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log('API Response:', data);
-                
-                // Format the date if formatDates is true
-                const formattedOptions = data.map(option => {
-                    if (formatDates) {
-                        const date = new Date(option);
-                        const days = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'];
-                        const months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
-                        
-                        const dayOfWeek = days[date.getDay()];
-                        const dayOfMonth = date.getDate();
-                        const month = months[date.getMonth()];
-                        
-                        return `${dayOfWeek} ${dayOfMonth} ${month}`;
-                    } else {
-                        return option;
-                    }
-                });
-                
-                setOptions(formattedOptions);
-            })
-            .catch(error => console.error(`Error fetching ${label}:`, error));
-    }, [label, formatDates]); // Assurez-vous d'inclure formatDates dans la liste de dépendances
+const Dropdown = ({
+  label,
+  variant,
+  onSelect,
+  formatDates,
+  className,
+}: DropdownProps) => {
+  const [options, setOptions] = useState<string[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+  const dropdownRef = useRef(null);
 
-    
-    
-    useEffect(() => {
-        const handleClickOutside = (event: MouseEvent) => {
-            if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-                setIsOpen(false);
-            }
-        };
-        
-        document.addEventListener('click', handleClickOutside);
-        
-        return () => {
-            document.removeEventListener('click', handleClickOutside);
-        };
-    }, []);
-    
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
-    
-    const handleSelect = (option: string) => {
-        onSelect(option);
-        setSelectedOption(option);
+  useEffect(() => {
+    console.log("Dropdown component mounted");
+    // Fetch options based on the label
+    fetch(`https://api.brm.lou-va.com/screenings/${label.toLowerCase()}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("API Response:", data);
+
+        // Format the date if formatDates is true
+        const formattedOptions = data.map((option: string) => {
+          if (formatDates) {
+            const date = new Date(option);
+            const days = [
+              "dimanche",
+              "lundi",
+              "mardi",
+              "mercredi",
+              "jeudi",
+              "vendredi",
+              "samedi",
+            ];
+            const months = [
+              "janvier",
+              "février",
+              "mars",
+              "avril",
+              "mai",
+              "juin",
+              "juillet",
+              "août",
+              "septembre",
+              "octobre",
+              "novembre",
+              "décembre",
+            ];
+
+            const dayOfWeek = days[date.getDay()];
+            const dayOfMonth = date.getDate();
+            const month = months[date.getMonth()];
+
+            return `${dayOfWeek} ${dayOfMonth} ${month}`;
+          } else {
+            return option;
+          }
+        });
+
+        setOptions(formattedOptions);
+      })
+      .catch((error) => console.error(`Error fetching ${label}:`, error));
+  }, [label, formatDates]); // Assurez-vous d'inclure formatDates dans la liste de dépendances
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
         setIsOpen(false);
+      }
     };
-    
-    return (
-        <div ref={dropdownRef} className={`relative inline-block w-1/12 ${className}`}>
-            <div
-                onClick={toggleDropdown}
-                className={`${
-                    variant === 'orange'
-                        ? 'bg-orangePrimary text-blackPrimary'
-                        : 'bg-blackPrimary text-orangePrimary'
-                } w-fit-content flex justify-between items-center rounded-40 py-[7px] px-30 cursor-pointer`}
-            >
-                {selectedOption || label}
-                <RxCaretDown/>
-            </div>
-            {isOpen && (
-                <div
-                    className={`custom-scrollbar mt-10 w-80 h-72 flex flex-col gap-4 absolute top-full left-0 bg-blackLighter border border-4 border-orangePrimary rounded-40 shadow-md z-50 overflow-auto`}
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelect = (option: string) => {
+    onSelect(option);
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div ref={dropdownRef} className={`  ${className}`}>
+      <div
+        onClick={toggleDropdown}
+        className={`${
+          variant === "orange"
+            ? "bg-orangePrimary text-blackPrimary"
+            : "bg-blackPrimary text-orangePrimary"
+        }  flex justify-between items-center rounded-40 py-[7px] px-30 cursor-pointer gap-2 relative`}
+      >
+        <Typography fontSize="20" fontFamily="Franklin" textColor="orange">
+          {selectedOption || label}
+        </Typography>
+        {isOpen ? <RxCaretUp size="25px" /> : <RxCaretDown size="25px" />}
+      </div>
+      {isOpen && (
+        <div className=" h-auto gap-4 absolute mt-4 ml-4 bg-blackLighter border-2 border-orangeLighter rounded-25 shadow-md z-50 p-4">
+          <div
+            className={
+              "custom-scrollbar w-max h-72 flex flex-col overflow-auto pr-6"
+            }
+          >
+            {options.map((option) => (
+              <div
+                key={option}
+                className="cursor-pointer hover:bg-orangePrimary w-full rounded-xl py-[7px] px-[20px]"
+                onClick={() => handleSelect(option)}
+              >
+                <Typography
+                  fontSize={"20"}
+                  textColor={"white"}
+                  fontFamily={"Franklin"}
                 >
-                    {options.map(option => (
-                        <div
-                            key={option}
-                            className="py-2 px-4 cursor-pointer hover:bg-orangePrimary"
-                            onClick={() => handleSelect(option)}
-                        >
-                            <Typography fontSize={'20'} textColor={'white'} fontFamily={'Franklin'}>
-                                {option}
-                            </Typography>
-                        </div>
-                    ))}
-                </div>
-            )}
+                  {option}
+                </Typography>
+              </div>
+            ))}
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Dropdown;
