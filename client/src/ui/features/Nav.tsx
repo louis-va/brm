88644d0b4f0b7ad722caadded0d4fce5bd0e2459
Alexander-fejari <alexander.fejari@gmail.com>
2023-12-15 11:01:31 +0100

    modif dropDown navBar

--- client/src/ui/features/Nav.tsx
+++ client/src/ui/features/Nav.tsx
@@ -1,75 +1,68 @@
-import { useState, useEffect, useRef } from "react";
-import { Logo } from "../design-system/Logo.tsx";
-import { Button } from "../design-system/Button.tsx";
-import { Typography } from "../design-system/Typography.tsx";
-import { FaAngleDown } from "react-icons/fa6";
-import { Link } from "react-router-dom";
-import { Auth } from "../features/Auth";
+import {useState, useEffect, useRef} from "react";
+import {Logo} from "../design-system/Logo.tsx";
+import {Button} from "../design-system/Button.tsx";
+import {Typography} from "../design-system/Typography.tsx";
+import {Link} from "react-router-dom";
+import {Auth} from "./Auth.tsx";
+import Dropdown from "../design-system/Dropdown.tsx";
 
 interface NavProps {
-  className?: string;
+    className?: string;
 }
 
-export const Nav = ({ className }: NavProps) => {
-  
-  const [showPopup, setShowPopup] = useState(false);
-  const popupRef = useRef<HTMLDivElement>(null);
-
-  const togglePopup = () => {
-    setShowPopup(!showPopup);
-  };
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
-      setShowPopup(false);
-    }
-  };
-
-  useEffect(() => {
-    if (showPopup) {
-      document.addEventListener("click", handleClickOutside, true);
-    } else {
-      document.removeEventListener("click", handleClickOutside, true);
-    }
-
-    return () => {
-      document.removeEventListener("click", handleClickOutside, true);
+export const Nav = ({className}: NavProps) => {
+    const [showPopup, setShowPopup] = useState(false);
+    const popupRef = useRef<HTMLDivElement>(null);
+    const [selectedGenre, setSelectedGenre] = useState('');
+    const [selectedDate, setSelectedDate] = useState('');
+    
+    const togglePopup = () => {
+        setShowPopup(!showPopup);
     };
-  }, [showPopup]);
-
-  return (
-    <nav
-      className={`${className} sticky top-0 w-full h-fit p-30 bg-orangePrimary flex flex-row justify-between items-center z-50`}
-    >
-      <Link to={"/"}>
-        <Logo size={"small"} />
-      </Link>
-      <div className="flex gap-5">
-        <Button variant="black">
-          <Typography fontSize="20" textColor="orange" fontFamily="Franklin">
-            Date
-          </Typography>
-          <FaAngleDown size="15px" />
-        </Button>
-        <Button variant="black">
-          <Typography fontSize="20" textColor="orange" fontFamily="Franklin">
-            Genre
-          </Typography>
-          <FaAngleDown size="15px" />
-        </Button>
-        <Button onClick={togglePopup} variant="orange" icon>
-          <Typography fontSize="20" textColor="black" fontFamily="Franklin">
-            Se connecter
-          </Typography>
-        </Button>
-        {showPopup && (
-          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50">
-            <div className="w-1/2" ref={popupRef}>
-              <Auth />
+    
+    const handleClickOutside = (event: MouseEvent) => {
+        if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
+            setShowPopup(false);
+        }
+    };
+    
+    useEffect(() => {
+        if (showPopup) {
+            document.addEventListener("click", handleClickOutside, true);
+        } else {
+            document.removeEventListener("click", handleClickOutside, true);
+        }
+        
+        return () => {
+            document.removeEventListener("click", handleClickOutside, true);
+        };
+    }, [showPopup]);
+    
+    
+    return (
+        <nav
+            className={`${className} sticky top-0 w-full h-fit p-30 bg-orangePrimary flex flex-row justify-between items-center z-50`}
+        >
+            <Link to={"/"}>
+                <Logo size={"small"}/>
+            </Link>
+            <div className="flex flex-row justify-end items-center w-full gap-5">
+                <Dropdown  label="Dates" variant="black" onSelect={setSelectedDate} formatDates={true}/>
+                <Dropdown label="Genres" variant="black" onSelect={setSelectedGenre} formatDates={false}/>
+                <Button className={''} onClick={togglePopup} variant="orange" icon>
+                    <Typography fontSize="20" textColor="black" fontFamily="Franklin">
+                        Se connecter
+                    </Typography>
+                </Button>
+                {showPopup && (
+                    <div
+                        className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50">
+                        <div className="w-1/2" ref={popupRef}>
+                            <Auth/>
+                        </div>
+                    </div>
+                )}
             </div>
-          </div>
-        )}
-      </div>
-    </nav>
-  );
+        </nav>
+    );
 };
