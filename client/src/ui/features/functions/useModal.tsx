f7a3ae1d08be81f9576c578b8b6f31e4ccdd1151
bSchutters <bryan021@hotmail.be>
2023-12-20 12:12:52 +0100

    fix closeModal

---
+++ client/src/ui/features/functions/useModal.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export const useModal = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  return {
+    showModal,
+    closeModal,
+    openModal,
+  };
+};
